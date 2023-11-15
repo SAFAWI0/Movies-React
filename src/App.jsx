3d9af979143c0b686862bcbc320a464f16fc54a5
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2023-11-16 02:06:32 +0300

    It works

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
 import  MovieCard from "./componts/moviecard/moviecard";
 import "../src/App.css";
+import img from "../assets/logo.png"
+//import "../src/componts/header/header.css";
 import { useEffect, useState } from "react";
-import MainHeader from "./componts/header/header";
+//import MainHeader from "./componts/header/header";
 
 const App = () => {
   const [movies, setmovies] = useState([]);
+  const [val, setval] = useState('');
 
-  const Ser="https://api.themoviedb.org/3/search/movie"
+  const Ser="https://api.themoviedb.org/3/search/movie?api_key=b6434bbf5557a52512008a50a1331ff7&query="
   const loadData = async () => {
     try {
       let resp = await fetch(
         "https://api.themoviedb.org/3/movie/popular?api_key=b6434bbf5557a52512008a50a1331ff7"
+        
       );
       let jsonData = await resp.json();
-      setmovies(jsonData);
+      setmovies(jsonData.results);
       console.log(jsonData);
     } catch (error) {
       console.log(error);
     }
   };
-
+console.log(movies)
   useEffect(() => {
     loadData();
   }, []);
-
+ const handleSearch = (e)=>{
+  e.preventDefault()
+  fetch(Ser+val)
+  .then(res=>res.json())
+  .then(data=> setmovies(data.results))
+ }
+ const handleClick=()=>{
+  
+ }
 
   return (
     <div className="container">
+       
+      <div className="header">
+      <img className="logo" src={img}  />
+          <form onSubmit={handleSearch}>
+          <input className="input" type="text" placeholder="Search" onChange={(e)=> setval(e.target.value)} />
+          <button className="buttom" onClick={ handleClick }>Gooo </button>
+          </form>
+        
+      </div>
+    
       
-
       <div className="movies">
-                                            {/* error */}
-        {/* {movies.map((el) => 
+        {movies.map((el) => 
           <MovieCard {...el} />
-        )} */}
-         <MainHeader/>
+        )}
       </div>
     </div>
   );
--- src/componts/moviecard/moviecard.js
+++ src/componts/moviecard/moviecard.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import "./moviecard.css"
 const Api = "https://image.tmdb.org/t/p/w500/";
  const MovieCard = (props) => {
+ 
   return (
     <div className='card'>
         <div className='poster'>
-            <img src={Api+props.poster_path}/>
+            <img src={props.poster_path? Api+props.poster_path:"https://images.unsplash.com/photo-1560109947-543149eceb16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNpbmVtYXxlbnwwfHwwfHx8MA%3D%3D"}/>
         </div>
         <div className='info'>
             <p className='title'>{props.title}</p>
             <p className='vote'>{props.vote_average}</p>
+        </div>
+        <div className='overview'>
+          <h2 className='title_overview'> Overview:</h2>
+          <h3 className='overview_info'>{props.overview}</h3>
         </div>
     </div>
   )
